c33f6638123f30ea2399185aef7267e7df69196f
rishabhCMS <rishabh.uniyal@cern.ch>
2020-10-20 12:34:32 -0400

    replace /tmp/x509up_u556950957 ---> /tmp/x509up_u556951020

--- analysis/run_condor.py
+++ analysis/run_condor.py
@@ -42,7 +42,7 @@ if options.cluster == 'kisti':
 Executable = run.sh
 Should_Transfer_Files = YES
 WhenToTransferOutput = ON_EXIT
-Transfer_Input_Files = run.sh, /tmp/x509up_u556950957
+Transfer_Input_Files = run.sh, /tmp/x509up_u556951020
 Output = hists/$ENV(PROCESSOR)/run_condor/out/$ENV(SAMPLE)_$(Cluster)_$(Process).stdout
 Error = hists/$ENV(PROCESSOR)/run_condor/err/$ENV(SAMPLE)_$(Cluster)_$(Process).stderr
 Log = hists/$ENV(PROCESSOR)/run_condor/log/$ENV(SAMPLE)_$(Cluster)_$(Process).log
