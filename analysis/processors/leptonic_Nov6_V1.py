1a0851253ead874c97f47295c0ae4bc9d13e55f4
rishabhCMS <rishabh.uniyal@cern.ch>
2020-11-08 18:18:16 -0500

    Electron trigger weights were wrong
    get_ele_trig_weight(leading_e.eta.sum()+leading_e.deltaEtaSC.sum(), leading_e.pt.sum())

--- analysis/processors/leptonic_Nov6_V1.py
+++ analysis/processors/leptonic_Nov6_V1.py
@@ -810,11 +810,11 @@ class AnalysisProcessor(processor.ProcessorABC):
                 sf[np.isnan(sf) | np.isinf(sf)] == 1
 
             trig = {
-                'sre' : get_met_trig_weight(leading_e.eta.sum(),leading_e.pt.sum()),
+                'sre' : get_ele_trig_weight(leading_e.eta.sum()+leading_e.deltaEtaSC.sum(), leading_e.pt.sum()),
                 'srm' : get_met_trig_weight(leading_mu.eta.sum(),leading_mu.pt.sum()),
-                'ttbare' : get_met_trig_weight(leading_e.eta.sum(),leading_e.pt.sum()),
+                'ttbare' : get_ele_trig_weight(leading_e.eta.sum()+leading_e.deltaEtaSC.sum(), leading_e.pt.sum()),
                 'ttbarm' : get_met_trig_weight(leading_mu.eta.sum(),leading_mu.pt.sum()),
-                'wjete' : get_met_trig_weight(leading_e.eta.sum(),leading_e.pt.sum()),
+                'wjete' : get_ele_trig_weight(leading_e.eta.sum()+leading_e.deltaEtaSC.sum(), leading_e.pt.sum()),
                 'wjetm' : get_met_trig_weight(leading_mu.eta.sum(),leading_mu.pt.sum()),
 #                 'dilepe' : get_met_trig_weight(leading_e.eta.sum(),leading_e.pt.sum()),
 #                 'dilepm' : get_met_trig_weight(leading_mu.eta.sum(),leading_mu.pt.sum()),
