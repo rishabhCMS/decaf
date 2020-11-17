b3d7860d307e4196566e270ad2c432077ec2ebfb
rishabhCMS <rishabh.uniyal@cern.ch>
2020-11-17 12:29:10 -0500

    Changed data sets
    removed: MET
    added SingleMuon
    
    I was using the wrong datasets for muon regions

--- analysis/data/process.py
+++ analysis/data/process.py
@@ -6,14 +6,14 @@ Just a giant list of processes and properties
 processes =    {
 
     #data
-    'MET':('MET','Data',1),
+    #'MET':('MET','Data',1),
     'EGamma':('EGamma','Data',1),
     #'MuonEG':('MuonEG','Data',1),
-    #'SingleMuon':('SingleMuon','Data',1),
+    'SingleMuon':('SingleMuon','Data',1),
     #'DoubleMuon':('DoubleMuon','Data',1),
     #'DoubleEG':('DoubleEG','Data',1),
-    'SingleElectron':('SingleElectron','Data',1),
-    'SinglePhoton':('SinglePhoton','Data',1),
+#     'SingleElectron':('SingleElectron','Data',1),
+#     'SinglePhoton':('SinglePhoton','Data',1),
     #'JetHT':('JetHT','Data',1),
     
     # inclusive NLO V+jets 
