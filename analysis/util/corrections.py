42978030a8eace505cd2f03b988976638db27923
Rishabh Uniyal <rishabh.uniyal@cern.ch>
2020-12-09 15:42:53 -0500

    Update corrections.py

--- analysis/util/corrections.py
+++ analysis/util/corrections.py
@@ -444,7 +444,7 @@ class BTagCorrector:
         down = zerotag(eff_data_down)/zerotag(eff)
 
         # Modified b tag Nov 25
-        @nb.njit()
+        @nb.njit
         def onetag(eff):
             output = np.zeros(eff.shape[0], np.float64)
             for event_num in range(eff.shape[0]):
