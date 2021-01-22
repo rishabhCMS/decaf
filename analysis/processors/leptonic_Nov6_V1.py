00161a3576812f748ae09b27af16ecd6c90b0dfc
Rishabh Uniyal <rishabh.uniyal@cern.ch>
2021-01-21 22:18:56 -0500

    setting the e-loose and mu_loose selection for deltaR

--- analysis/processors/leptonic_Nov6_V1.py
+++ analysis/processors/leptonic_Nov6_V1.py
@@ -634,11 +634,11 @@ class AnalysisProcessor(processor.ProcessorABC):
         Delta_Phi_Met_LJ = (abs(met.T.delta_phi(leading_j.T)).min())>1.5
 
         # *******calculate deltaR( leading ak4jet, e/mu) < 3.4 *****
-        LJ_Ele = leading_j['p4'].cross(leading_e['p4'])
+        LJ_Ele = leading_j['p4'].cross(e_loose['p4'])
         DeltaR_LJ_Ele = LJ_Ele.i0.delta_r(LJ_Ele.i1)
         DeltaR_LJ_Ele_mask = (abs(DeltaR_LJ_Ele).max() < 3.4)
 
-        LJ_Mu = leading_j['p4'].cross(leading_mu['p4'])
+        LJ_Mu = leading_j['p4'].cross(mu_loose['p4'])
         DeltaR_LJ_Mu = LJ_Mu.i0.delta_r(LJ_Mu.i1)
         DeltaR_LJ_Mu_mask = (abs(DeltaR_LJ_Mu).max() < 3.4)
 
