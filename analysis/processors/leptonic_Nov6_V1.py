9f3e35edfea0f55ac68dba59524fb251779bb56c
Rishabh Uniyal <rishabh.uniyal@cern.ch>
2021-04-22 12:19:29 -0400

    new single lele triggers

--- analysis/processors/leptonic_Nov6_V1.py
+++ analysis/processors/leptonic_Nov6_V1.py
@@ -204,7 +204,9 @@ class AnalysisProcessor(processor.ProcessorABC):
         self._singleelectron_triggers = {  # 2017 and 2018 from monojet, applying dedicated trigger weights
             '2016': [
                 'Ele27_WPTight_Gsf',
-                'Ele105_CaloIdVT_GsfTrkIdT'
+                #'Ele105_CaloIdVT_GsfTrkIdT', https://twiki.cern.ch/twiki/bin/view/CMS/EgHLTRunIISummary
+                'Ele115_CaloIdVT_GsfTrkIdT'
+                'HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165'
             ],
             '2017': [
                 'Ele35_WPTight_Gsf',
