17a6502475907214eb6924bec8ecac136afe6a76
Rishabh Uniyal <rishabh.uniyal@cern.ch>
2021-04-27 13:55:18 -0400

    minor typo

--- analysis/processors/leptonic_Nov6_V1.py
+++ analysis/processors/leptonic_Nov6_V1.py
@@ -1336,8 +1336,8 @@ class AnalysisProcessor(processor.ProcessorABC):
                             dataset=dataset, sumw=1, weight=events.genWeight.sum())
                         isFilled = True
                     cut = selection.all(*regions[region])
-#                     for systematic in [None]:
-                    for systematic in [None, 'btagUp', 'btagDown']:
+                    for systematic in [None]:
+#                     for systematic in [None, 'btagUp', 'btagDown']:
                         sname = 'nominal' if systematic is None else systematic
                         hout['template'].fill(dataset=dataset,
                                               region=region,
