674b09846df96b01bf16a20325d7f6e56b45616b
Rishabh Uniyal <rishabh.uniyal@cern.ch>
2021-06-09 10:58:30 -0400

    new changes
    1. introducing DeltaR_LJ_Mu_mask = (abs(DeltaR_LJ_Mu).max() < 3.4) for electrons and muons
    2 add dphi met and leading jet (It was already there)

--- analysis/processors/leptonic_Nov6_V1.py
+++ analysis/processors/leptonic_Nov6_V1.py
@@ -648,9 +648,11 @@ class AnalysisProcessor(processor.ProcessorABC):
         # *******calculate deltaR( leading ak4jet, e/mu) < 3.4 *****
         LJ_Ele = leading_j['p4'].cross(e_loose['p4'])
         DeltaR_LJ_Ele = LJ_Ele.i0.delta_r(LJ_Ele.i1)
-        DeltaR_LJ_Ele_mask = (abs(DeltaR_LJ_Ele).max() < 3.4)
+        #DeltaR_LJ_Ele_mask = (abs(DeltaR_LJ_Ele).max() < 3.4)
+        DeltaR_LJ_Ele_mask = (abs(DeltaR_LJ_Ele).max() < 1.7)
         LJ_Mu = leading_j['p4'].cross(mu_loose['p4'])
         DeltaR_LJ_Mu = LJ_Mu.i0.delta_r(LJ_Mu.i1)
+        #DeltaR_LJ_Mu_mask = (abs(DeltaR_LJ_Mu).max() < 3.4)
         DeltaR_LJ_Mu_mask = (abs(DeltaR_LJ_Mu).max() < 3.4)
 
         ele_pairs = e_loose.distincts()
@@ -1160,7 +1162,7 @@ class AnalysisProcessor(processor.ProcessorABC):
                 'ele_pT':              e_tight.pt,
                 'jet_pT':              leading_j.pt,
                 'metphi':                 met.phi,
-                # 'mindphimet':             abs(met.T.delta_phi(j_clean.T)).min(),
+                'dphi_Met_LJ':             abs(met.T.delta_phi(j_clean.T)).min(),
                 # 'j1pt':                   leading_j.pt,
                 # 'j1eta':                  leading_j.eta,
                 # 'j1phi':                  leading_j.phi,
